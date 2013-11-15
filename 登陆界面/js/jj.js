5469ec258c54d96f19566f92e1fcd7d2f1243246
fanfzj <1009137312@qq.com>
2013-11-15 13:34:35 +0800

    Exam更新文件

---
+++ 登陆界面/js/jj.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	
+	$("#bt").blur(function(){
+		  alert("hellowprld");
+		});
+	$("#bt").focus(function(){
+		  alert("hellowprld");
+		});
+});
+
