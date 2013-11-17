6c0e0411f7749aee2c7a62509b329fa461111b5e
fanfzj <1009137312@qq.com>
2013-11-17 11:21:02 +0800

    更新

---
+++ VB/在线考试系统/在线考试系统/登陆.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class 登陆
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(登陆))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextName = New System.Windows.Forms.TextBox()
+        Me.TextPasssword = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, 58)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(802, 139)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.Control
+        Me.Label1.Font = New System.Drawing.Font("方正正大黑简体", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(166, 257)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(131, 36)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "用户名："
+        '
+        'TextName
+        '
+        Me.TextName.Location = New System.Drawing.Point(341, 257)
+        Me.TextName.Name = "TextName"
+        Me.TextName.Size = New System.Drawing.Size(270, 21)
+        Me.TextName.TabIndex = 2
+        '
+        'TextPasssword
+        '
+        Me.TextPasssword.Location = New System.Drawing.Point(341, 336)
+        Me.TextPasssword.Name = "TextPasssword"
+        Me.TextPasssword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextPasssword.Size = New System.Drawing.Size(269, 21)
+        Me.TextPasssword.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("方正正大黑简体", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(180, 323)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 36)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "密码："
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(216, 449)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 37)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "登陆"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(515, 449)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(68, 37)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "退出"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(630, 266)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 12)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "用户名验证"
+        Me.Label3.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(630, 339)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 12)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "密码验证"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"老师", "学生"})
+        Me.ComboBox1.Location = New System.Drawing.Point(349, 394)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(260, 20)
+        Me.ComboBox1.TabIndex = 9
+        Me.ComboBox1.Text = "身份选择"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Cursor = System.Windows.Forms.Cursors.No
+        Me.Label5.Font = New System.Drawing.Font("方正正大黑简体", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label5.Location = New System.Drawing.Point(181, 384)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 36)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "身份："
+        '
+        '登陆
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(802, 549)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextPasssword)
+        Me.Controls.Add(Me.TextName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "登陆"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "登陆"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextName As System.Windows.Forms.TextBox
+    Friend WithEvents TextPasssword As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ VB/在线考试系统/在线考试系统/登陆.vb
@@ -0,0 +1,59 @@
+﻿
+Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+Public Class 登陆
+
+    Private Sub Form_Unload(Cancel As Integer)
+        If MsgBox("你选择了退出系统登录, 退出将不能启动管理系统!" & vbCrLf _
+        & "是否真的退出？", vbYesNo + vbQuestion, "登录验证") = vbYes Then
+            End '卸载登录窗体
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        '请求用户确认是否真的退出系统登录
+        If MsgBox("你选择了退出系统登录, 退出将不能启动管理系统!" & vbCrLf _
+        & "是否真的退出？", vbYesNo + vbQuestion, "登录验证") = vbYes Then
+            End'卸载登录窗体
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Label1.BackColor = Color.Transparent
+        Label2.BackColor = Color.Transparent
+        Label5.BackColor = Color.Transparent
+        '清空用户名和口令文本框
+        TextName.Text = ""
+        TextPasssword.Text = ""
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sqlConnection1 As SqlClient.SqlConnection
+        sqlConnection1 = New System.Data.SqlClient.SqlConnection("Server=(local);database=(在线考试系统);uid=fanfzj;pwd=123456;")  '“server=计算机名;database=数据库名;uid=登录数据库用户名;pwd=登录数据库密码”  )
+        sqlConnection1.Open()         '打开数据库  
+
+
+        '验证代码  
+        Dim sql As String = "select count(*) from UserInfo where 用户名='" & TextName.Text.Trim() & "' and 密码='" & TextPasssword.Text() & "'"
+        Dim sqlCmd As SqlCommand = New SqlCommand(sql, sqlConnection1)
+
+        Dim count As Integer
+        count = sqlCmd.ExecuteScalar
+
+        If count > 0 Then
+            MsgBox("登陆成功!")
+        Else
+            MsgBox("登陆失败!")
+        End If
+
+        sqlConnection1.Close()     '关闭数据库  
+        Static intLogTimes As Integer                    '验证登陆次数
+        intLogTimes = intLogTimes + 1
+        If intLogTimes > 3 Then
+            MsgBox("你已经超过允许的登录验证次数！" & vbCr _
+                 & "应用程序将结束！", vbCritical, "登录验证")
+            End                                          '结束应用程序
+        End If
+    End Sub
+End Class
+
